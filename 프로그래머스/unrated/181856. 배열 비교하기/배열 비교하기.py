afb10ca924505b137d629c00df84b3bd0496166c
wodyd1421 <143332609+wodyd1421@users.noreply.github.com>
2023-12-30 20:25:01 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,2 @@
+def solution(arr1, arr2):
+    return int(bool((len(arr1)-len(arr2))/abs(len(arr1)-len(arr2))+1))*2-1 if len(arr1) != len(arr2) else int(bool((sum(arr1)-sum(arr2))/abs(sum(arr1)-sum(arr2))+1))*2-1 if sum(arr1) != sum(arr2) else 0
