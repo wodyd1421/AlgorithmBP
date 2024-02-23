69ca4571ea5af8d726e3285a797d2033892347a2
wodyd1421 <143332609+wodyd1421@users.noreply.github.com>
2024-02-23 16:50:50 +0900

    [Bronze I] Title: 임시 반장 정하기, Time: 444 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = []
+prev = -1
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+for i in range(n):
+    ct = -1
+    for j in range(n):
+        if (arr[i][0] == arr[j][0]) or (arr[i][1] == arr[j][1]) or (arr[i][2] == arr[j][2]) or (arr[i][3] == arr[j][3]) or (arr[i][4] == arr[j][4]):
+            ct += 1
+    if prev < ct:
+        prev = ct
+        m = i + 1
+print(m)
